48cb9839062389911abdad7c83df3ec4fc5a7839
DTZxPorter <dtzxporter@gmail.com>
2019-03-23 18:20:40 -0400

    v1.0.1
    Runtime patching of DDS structure, no more installer required.

--- README.md
+++ README.md
@@ -5,24 +5,14 @@ A less shitty DDS plugin for Paint.NET
 
 - Read just about any DDS file (NEW DDS FILES SUPPORTED)
 - Write the DDS back to a file
-- Fast performance, easy to use installer
+- Faster DDS performance all around
 
 ## Installation:
 
 Download the latest [Release.zip](https://github.com/dtzxporter/FileTypeDDS/releases) and extract the archive.
 
-Next, launch `FileTypeDDSInstaller.exe` and run as admin if prompted to do so. Click `install` and wait, this will only take a few moments.
-
-You may now launch Paint.NET and open a DDS file.
+Next, follow the instructions in `README.txt` to properly install it.
 
 ## Uninstall:
 
-Relaunch `FileTypeDDSInstaller.exe` and run as admin if prompted to do so. Click `uninstall` and wait, this will only take a few moments.
-
-## Notes:
-
-When Paint.NET has an update, you `must` re-run the installer, it will ensure that the plugin is up-to-date and ready to go.
-
-This plugin is provided as-is with no warranty provided. Use at your own risk.
-
-Wanna help? Send a message to `Rick Brewster` over at [https://forums.getpaint.net/](https://forums.getpaint.net/) asking for the ability to override default filetypes via plugins! This will make it easier to use our custom DDS plugin, without the need for an installer!
+Just delete all the files you have copied over.
---
+++ src/FileTypeDDS/FileTypeDDSEffect/PatcherEffect.cs
@@ -0,0 +1,52 @@
+﻿using PaintDotNet;
+using PaintDotNet.Effects;
+using PaintDotNet.PropertySystem;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTypeDDSEffect
+{
+    [EffectCategory(EffectCategory.DoNotDisplay), PluginSupportInfo(typeof(PluginSupportInfo), DisplayName = "FileTypeDDS")]
+    public class PatcherEffect : PropertyBasedEffect
+    {
+        public PatcherEffect()
+            : base("FileTypeDDSPatcher", null, "", EffectFlags.None)
+        {
+            try
+            {
+                // Prepare to patch the built-in file type array
+                var PaintAssembly = Assembly.GetEntryAssembly();
+
+                // Find the internal file type array
+                var Type = PaintAssembly.GetType("PaintDotNet.Data.PdnFileTypes");
+                var Field = Type.GetField("fileTypes", BindingFlags.NonPublic | BindingFlags.Static);
+
+                // Get it's value, then, replace with one that doesn't have the DDS
+                var Result = (FileType[])Field.GetValue(null);
+                var NewResult = Result.Where(x => !x.Name.ToLower().Contains("dds")).ToArray<FileType>();
+
+                // Patch it, we want full access...
+                Field.SetValue(null, NewResult);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("FileTypeDDS - Failed to patch the plugin (Report this on Github) [" + ex.Message + "]", "FileTypeDDS");
+            }
+        }
+
+        protected override void OnRender(Rectangle[] rois, int startIndex, int length)
+        {
+            // Nothing...
+        }
+
+        protected override PropertyCollection OnCreatePropertyCollection()
+        {
+            return new PropertyCollection(new List<Property>());
+        }
+    }
+}
